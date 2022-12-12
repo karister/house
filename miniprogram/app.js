d8df1926d244eef097893a4ea5931fdd87744070
yuqing <karister@qq.com>
2022-12-12 22:05:30 +0800

    fix network error

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,7 +6,7 @@ App({
             console.error('请使用 2.2.3 或以上的基础库以使用云能力')
         } else {
             wx.cloud.init({
-                env: 'house-rent-8g5gkazeabb6f47b',
+                env: 'house-rent-4g2zb051ba3e643c',
                 traceUser: true,
             })
         }
